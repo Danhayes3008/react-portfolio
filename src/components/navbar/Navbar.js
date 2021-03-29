89875ca6b557c78ed9994156ce5fbea57bbf4959
danhayes <danhayes3008@outlook.com>
2021-03-29 13:22:37 +0100

    renamed the Name css class

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
       <nav>
-          <div className='Name'>
+          <div className='title'>
               <a href="#">Daniel Hayes</a>
           </div>
 

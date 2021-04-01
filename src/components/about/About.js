45736fd364f8349d932d4938b06b0a31b7374cde
danhayes <danhayes3008@outlook.com>
2021-04-01 12:24:15 +0100

    added a profile image

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Image from '../../styles/img/me_temp.png'
 
 const About = () => {
     return (
         <div className='about'>
             <h1>About</h1>
+            <img src={Image} />
         </div>
     )
 }

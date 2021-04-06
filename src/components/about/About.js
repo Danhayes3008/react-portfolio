461e8c3e66aab4e446f3820076bb060b66891a51
danhayes <danhayes3008@outlook.com>
2021-04-06 13:02:01 +0100

    added the new class name

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,7 +4,7 @@ import Image from '../../styles/img/me_temp.png'
 const About = () => {
     return (
         <div className='about'>
-            <div>
+            <div className='abt-cont'>
                 <h1 className='name'>Daniel Hayes</h1>
             </div>
             <div>

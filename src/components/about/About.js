390904b991044088c8e73774024ee796544d7d11
danhayes <danhayes3008@outlook.com>
2021-04-06 11:33:33 +0100

    wrapped the about h1 in a div so i can better style the about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,7 +4,9 @@ import Image from '../../styles/img/me_temp.png'
 const About = () => {
     return (
         <div className='about'>
-            <h1>About</h1>
+            <div>
+                <h1>About</h1>
+            </div>
             <img src={Image} />
         </div>
     )

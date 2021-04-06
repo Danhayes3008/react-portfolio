ad95b3080261d02af5cb2be9bf4adf8ee824758d
danhayes <danhayes3008@outlook.com>
2021-04-06 15:48:17 +0100

    added a paragraph to my about me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,6 +6,12 @@ const About = () => {
         <div className='about'>
             <div className='abt-cont'>
                 <h1 className='name'>Daniel Hayes</h1>
+                <p>
+                    As someone who is new to the industry I am looking for the right place to hone my skills and become
+                    a expert in React and other JavaScript based Languages and Frameworks. I also enjoy working With Python
+                    and am willing to learn what ever is needed to help both progress my career and ability to contribute to the
+                    team.
+                </p>
                 <img src="https://ghchart.rshah.org/76EE00/danhayes3008" alt="2016rshah's Blue Github Chart" />
             </div>
             <div className='abt-img'>

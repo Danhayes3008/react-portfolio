19c59b0c05edb4a5f841aeda9ce02c7223f4a4ed
danhayes <danhayes3008@outlook.com>
2021-04-08 11:59:30 +0100

    created a list of my skills

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Skills = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="Skills-title">Skills</h1>
+      <div>
+      <ul>
+          <li className="skill">HTML</li>
+          <li className="skill">JavaScript</li>
+          <li className="skill">Python</li>
+          <li className="skill">Database</li>
+      </ul>
+    </div>
+    </div>
+
+  )
 };
 
 export default Skills;

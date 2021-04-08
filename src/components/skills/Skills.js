ac87f46aaf15bae204f73b70ac0610ca139f566b
danhayes <danhayes3008@outlook.com>
2021-04-08 15:16:27 +0100

    rewrote my skills section to attempt to get the look i am after

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -7,7 +7,16 @@ const Skills = () => {
         <h1 className="Skills-title">Skills</h1>
       </div>
       <div className='skill'>
+        <span className='skills'>HTML5</span>
+        <span className='skills'>JavaScript</span>
+        <span className='skills'>Python</span>
+        <span className='skills'>Database</span>
       </div>
+      <div className='skill-sub-list'>
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>SASS</p>
+        </div>
     </div>
   );
 };

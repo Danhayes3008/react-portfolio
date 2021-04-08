febb202b75688848f7849c669bae16aacfb26420
danhayes <danhayes3008@outlook.com>
2021-04-08 14:13:36 +0100

    list method was not working so deleted it

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -6,22 +6,7 @@ const Skills = () => {
       <div>
         <h1 className="Skills-title">Skills</h1>
       </div>
-
-      <div>
-        <ul>
-          <li className="skill">
-            <p>HTML5</p>
-          </li>
-          <li className="skill">
-            <p>JavaScript</p>
-          </li>
-          <li className="skill">
-            <p>Python</p>
-          </li>
-          <li className="skill">
-          <p>Databases</p>
-            </li>
-        </ul>
+      <div className='skill'>
       </div>
     </div>
   );

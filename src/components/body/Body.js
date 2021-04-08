7319df3c134693b6393d3bef159d488bc270ed62
danhayes <danhayes3008@outlook.com>
2021-04-08 12:01:50 +0100

    imported the Skills component

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import About from '../about/About'
+import Skills from '../skills/Skills'
 const Body = () => {
     return (
         <div className='body'>
             <About />
+            <Skills />
         </div>
     )
 }

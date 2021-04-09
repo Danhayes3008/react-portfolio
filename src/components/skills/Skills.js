1793b07c8859f5bb50759d5ad1c678adde209fc1
danhayes <danhayes3008@outlook.com>
2021-04-09 14:13:51 +0100

    turned my spans into h3 tags and moved the subjects class

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -7,17 +7,17 @@ const Skills = () => {
         <h1 className="Skills-title">Skills</h1>
       </div>
       <div className='skill'>
-        <div>
-          <h3 className='subjects'>HTML5</h3>
+        <div className='subjects'>
+          <h3>HTML5</h3>
         </div>
-        <div>
-          <span className='subjects'>JavaScript</span>
+        <div className='subjects'>
+          <h3>JavaScript</h3>
         </div>
-        <div>
-          <span className='subjects'>Python</span>
+        <div className='subjects'>
+          <h3>Python</h3>
         </div>
-        <div>
-          <span className='subjects'>Database</span>
+        <div className='subjects'>
+          <h3>Database</h3>
         </div>
       </div>
     </div>

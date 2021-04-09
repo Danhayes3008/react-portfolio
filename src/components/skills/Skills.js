d6741532bf3a089852fc6ec62b142a95280cdaaf
danhayes <danhayes3008@outlook.com>
2021-04-09 15:05:52 +0100

    changed the quotation marks used and dealt with some whitespaces

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -2,22 +2,42 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <div className='skills '>
+    <div className="skills ">
       <div>
         <h1 className="Skills-title">Skills</h1>
       </div>
-      <div className='skill'>
-        <div className='subjects'>
+      <div className="skill">
+        <div className="subjects">
           <h3>HTML5</h3>
+          <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>scss</li>
+          </ul>
         </div>
-        <div className='subjects'>
+        <div className="subjects">
           <h3>JavaScript</h3>
+          <ul>
+            <li>ES6</li>
+            <li>React</li>
+            <li>Node</li>
+            <li>Electron</li>
+        </ul>
         </div>
-        <div className='subjects'>
+        <div className="subjects">
           <h3>Python</h3>
+          <ul>
+          <li>Flask</li>
+          <li>Django</li>
+        </ul>
         </div>
-        <div className='subjects'>
+        <div className="subjects">
           <h3>Database</h3>
+          <ul>
+          <li>SQLite3</li>
+          <li>MongoDB</li>
+          <li>JSON</li>
+        </ul>
         </div>
       </div>
     </div>

2038c5ae8cd94fad7d3d9b64be74bd56ac9681c7
danhayes <danhayes3008@outlook.com>
2021-04-16 12:25:30 +0100

    made a list to contain all my skills and removed the old code so i can use a new structure for my code

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,50 +1,37 @@
 import React from "react";
+import { useState } from 'react'
+ import Skill from './Skill'
 
 const Skills = () => {
+  const [skills] = useState([
+    {
+      id: 1,
+      text: 'HTML5',
+      persentage: 80
+    },
+    {
+      id: 2,
+      text: 'JavaScript',
+      persentage: 75
+    },
+    {
+      id: 3,
+      text: 'Python',
+      persentage: 45
+    },
+    {
+      id: 4,
+      text: 'Databases',
+      persentage: 35
+    },
+  ])
   return (
-    <div className="skills ">
-      <div>
-        <h1 className="Skills-title">Skills</h1>
-      </div>
-      <div className="skill">
-        <div className="subjects">
-          <div>
-            <span>HTML5</span>
-            <span className="html-progress">80%</span>
-          </div>
-          <div className="progressbar">
-            <div className="html"></div>
-          </div>
-        </div>
-        <div className="subjects">
-          <div>
-            <span>JavaScript</span>
-            <span className="js-progress">90%</span>
-          </div>
-          <div className="progressbar">
-            <div className="JavaScript"></div>
-          </div>
-        </div>
-        <div className="subjects">
-          <div>
-            <span>Python</span>
-            <span className="python-progress">45%</span>
-          </div>
-          <div className="progressbar">
-            <div className="Python"></div>
-          </div>
-        </div>
-        <div className="subjects">
-          <div>
-            <span>Database</span>
-            <span className="db-progress">35%</span>
-          </div>
-          <div className="progressbar">
-            <div className="Database"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+            {skills.map((skill) => (
+                <Skill key={skill.id} skills={skill} />
+            ))}
+        </>
+    
   );
 };
 

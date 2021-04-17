9057542e79ee23dc3e13a19c653895da043520bd
danhayes <danhayes3008@outlook.com>
2021-04-17 13:05:29 +0100

    imported the projects file

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import About from '../about/About'
 import Skills from '../skills/Skills'
+import Projects from '../projects/Projects'
 const Body = () => {
     return (
         <div className='body'>
             <About />
             <Skills />
+            <Projects />
         </div>
     )
 }

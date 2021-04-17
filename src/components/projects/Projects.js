1fdff0acc1d736fbb387cf3e71f51b5f6cf06f4a
danhayes <danhayes3008@outlook.com>
2021-04-17 14:20:38 +0100

    added to the list and started working on displaying my project template i have imported

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Project from "./Project"
 
 const Projects = () => {
   const [project] = useState([
@@ -8,12 +9,28 @@ const Projects = () => {
       name: "Project 1",
       photo: "../../styles/img/me_temp.png",
       text: "Coming Soon"
-    }
+    },
+    {
+      id: 2,
+      name: "Project 2",
+      photo: "../../styles/img/me_temp.png",
+      text: "Coming Soon"
+    },
+    {
+      id: 3,
+      name: "Project 3",
+      photo: "../../styles/img/me_temp.png",
+      text: "Coming Soon"
+    },
   ])
   return (
     <div className="projects">
       <h1>Projects</h1>
-      <p>welcome all</p>
+      <>
+      {project.map((project) => (
+          <Project key={project.id} project={project} />
+        ))}
+      </>
     </div>
   );
 };

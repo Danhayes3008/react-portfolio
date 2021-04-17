27119cffa03a53a3eb034a24d0d9d88146c2f236
danhayes <danhayes3008@outlook.com>
2021-04-17 14:15:34 +0100

    created a useState to hold the details about my project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 const Projects = () => {
+  const [project] = useState([
+    {
+      id: 1,
+      name: "Project 1",
+      photo: "../../styles/img/me_temp.png",
+      text: "Coming Soon"
+    }
+  ])
   return (
     <div className="projects">
       <h1>Projects</h1>

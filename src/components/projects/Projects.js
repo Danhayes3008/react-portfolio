ae3ca57870b686166f8733a54f976786d8dc556c
danhayes <danhayes3008@outlook.com>
2021-04-18 15:38:28 +0100

    did some work to get images to show up with my projects from the list i made

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,39 +1,40 @@
 import React from "react";
 import { useState } from "react";
-import Project from "./Project"
+import Project from "./Project";
+import images from "./ProjectImages";
 
 const Projects = () => {
   const [project] = useState([
     {
-      "id": 1,
-      "name": "Project 1",
-      "imageUrl": 'https://unsplash.com/s/photos/city',
+      id: 1,
+      name: "Project 1",
+      src: images.Image,
       text: "Coming Soon",
     },
     {
       id: 2,
       name: "Project 2",
-      poster: 'https://unsplash.com/s/photos/city',
+      src: images.Thanos,
       text: "Coming Soon",
     },
     {
       id: 3,
       name: "Project 3",
-      Poster: 'https://unsplash.com/s/photos/city',
-      text: "Coming Soon"
+      imageUrl: require("../../styles/img/me_temp.png"),
+      text: "Coming Soon",
     },
     {
       id: 4,
       name: "Project 4",
-      Poster: 'https://unsplash.com/s/photos/city',
-      text: "Coming Soon"
+      image: "../../styles/img/me_temp.png",
+      text: "Coming Soon",
     },
-  ])
+  ]);
   return (
     <div className="projects">
       <h1 className="project-title">Projects</h1>
       <>
-      {project.map((project) => (
+        {project.map((project) => (
           <Project key={project.id} project={project} />
         ))}
       </>

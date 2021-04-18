0803e357c31f4722ace7e9dd7f02d76e061ee189
danhayes <danhayes3008@outlook.com>
2021-04-18 15:52:00 +0100

    changed the source of the image

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -4,7 +4,7 @@ const Project = ({project}) => {
     return (
         <div className="project">
             <h1 className="project-name">{project.name}</h1>
-            <img src={project.Poster} alt='project'></img>
+            <img src={project.src} alt='project'></img>
             <p>{project.text}</p>
         </div>
     )

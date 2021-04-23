75f6960ded514a71ccdbe68b60c23bc9a6dc2cb8
danhayes <danhayes3008@outlook.com>
2021-04-23 11:23:40 +0100

    added the contact section

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -2,12 +2,14 @@ import React from 'react'
 import About from '../about/About'
 import Skills from '../skills/Skills'
 import Projects from '../projects/Projects'
+import Contact from '../contact/Contact'
 const Body = () => {
     return (
         <div className='body'>
             <About />
             <Skills />
             <Projects />
+            <Contact />
         </div>
     )
 }

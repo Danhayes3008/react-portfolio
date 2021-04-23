25bffa9a8c71cb5ed45cca2c6b273e2a567ea03e
danhayes <danhayes3008@outlook.com>
2021-04-23 12:33:40 +0100

    started working on the contact me section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="contact">
-
+            <div className="contact-content">
+                <h1>Get in Touch</h1>
+            </div>
+            <div className="contact-img">
+            </div>
         </div>
     )
 }
